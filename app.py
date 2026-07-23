e0bf4f168ae582b883e2512be49f6533aae98993
parveenshakya001 <parveenshakya969@gmail.com>
2026-07-23 13:34:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ with gr.Blocks(theme=gr.themes.Soft(primary_hue="blue", neutral_hue="slate")) as
     gr.Markdown("""
     ---
     ### 👨‍💻 About the Developer
-    **Created by:** Chandan Saroj (MERN Stack Developer & SDE)
+    **Created by:** Parveen
     * **LinkedIn:** [Connect with me](YOUR_LINKEDIN_URL_HERE)
     * **GitHub:** [Check out my projects](YOUR_GITHUB_URL_HERE)
     """)
